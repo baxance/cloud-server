1b0d350645a94ace47fbb4f968d7cb8040bd39a7
Barrett <baxance@protonmail.com>
2021-11-08 16:49:42 -0800

    GUI and CLI apps running and readme updated

--- server.js
+++ server.js
@@ -15,25 +15,21 @@ app.listen(PORT, () => {
 /*
 
 Choose a server you’ve built previously
-Option 1: A simple API or Web Server X
-Option 2: A socket.io event Hub
-The server should not require a database X
-Check in your server to GitHub X
+  X Option 1: A simple API or Web Server
+  Option 2: A socket.io event Hub
+  X The server should not require a database
+  X Check in your server to GitHub
 
 Task 1:
 
-Create a new environment, using Elastic Beanstalk from the AWS Control Panel (GUI)
-Manually deploy your application to this environment by uploading a .zip file
+  X Create a new environment, using Elastic Beanstalk from the AWS Control Panel (GUI)
+
+  X Manually deploy your application to this environment by uploading a .zip file
 
 Task 2:
 
-Using the same server, create a new environment using Elastic Beanstalk from your terminal
-Manually deploy your application to this environment by using eb deploy
+  X Using the same server, create a new environment using Elastic Beanstalk from your terminal
 
-what do i need to run besides `npm install` when I'm getting something off the ground to fill out the package.json?
-
-npm init dumps a bunch of dependencies into the package.json seemingly randomly
-
-how do I get a clean package.json 
+  X Manually deploy your application to this environment by using eb deploy
 
 */
